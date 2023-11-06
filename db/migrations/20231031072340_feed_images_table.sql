c7a9ccd94bb4ebc86981404ea0af706891f1b17a
namsuhan0322 <rnatpakfl@gmail.com>
2023-11-06 19:49:59 +0900

    이미지 db수정

--- db/migrations/20231031072340_feed_images_table.sql
+++ db/migrations/20231031072340_feed_images_table.sql
@@ -1,8 +1,16 @@
 -- migrate:up
 CREATE TABLE `feed_images` (
   `id` int PRIMARY KEY NOT NULL,
-  `url` varchar(512) NOT NULL,
-  `text` varchar(500),
+  `url1` varchar(512) NULL,
+  `text1` varchar(500),
+  `url2` varchar(512) NULL,
+  `text2` varchar(500),
+  `url3` varchar(512) NULL,
+  `text3` varchar(500),
+  `url4` varchar(512) NULL,
+  `text4` varchar(500),
+  `url5` varchar(512) NULL,
+  `text5` varchar(500),
   `feed_id` int NOT NULL,
   FOREIGN KEY (`feed_id`) REFERENCES `feeds` (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
