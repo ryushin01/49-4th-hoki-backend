ad5837e5e569325461465f1296654e70a5aa22d8
namsuhan0322 <rnatpakfl@gmail.com>
2023-11-08 15:01:01 +0900

    AWS S3 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+
+const testRouter = require('./testRouter');
+
 const router = express.Router();
+
+router.use('/test', testRouter);
 
 router.get('/', (req, res) => {
   res.send('OK');
